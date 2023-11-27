50a79dd16794133cc7e8bf9bb9ccc98b07221180
BadAimWeeb <badaimweeb@protonmail.com>
2023-11-27 00:36:50 -0600

    1.0.2: use sha512 null constant

--- src/utils.ts
+++ src/utils.ts
@@ -144,4 +144,13 @@ export function exactArray<T extends ArrayLike<any>>(a: T, b: T) {
     return true;
 }
 
-export const SHA512_NULL = new Uint8Array(await crypto.subtle.digest("SHA-512", new Uint8Array(0)));
+export const SHA512_NULL = Uint8Array.from([
+    207, 131, 225, 53, 126, 239, 184, 189,
+    241, 84, 40, 80, 214, 109, 128, 7,
+    214, 32, 228, 5, 11, 87, 21, 220,
+    131, 244, 169, 33, 211, 108, 233, 206,
+    71, 208, 209, 60, 93, 133, 242, 176,
+    255, 131, 24, 210, 135, 126, 236, 47,
+    99, 185, 49, 189, 71, 65, 122, 129,
+    165, 56, 50, 122, 249, 39, 218, 62
+]);
